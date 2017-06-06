cc0b855f277acc5bffbe3fb4af72f554547e969f
Anatole Dahan <adahan@me.com>
2017-06-06 16:38:19 +0200

    Initial Commit
    See README

---
+++ parser.ml
@@ -0,0 +1,68 @@
+exception Unvalid_atom of string
+exception Unvalid_line of string*string
+exception Parsing_failed of int*exn
+exception Wrong_number_of_clauses of int
+exception Incorrect_initialization of string
+                                        
+let atom_parser str = try
+    match String.split_on_char '=' str with
+    |hd1::hd2::[] -> Equal((int_of_string hd1),(int_of_string hd2))
+    |hd::[] ->
+      begin match String.split_on_char '<' str with
+      |hd1::hd2::[] -> begin match String.get hd2 0 with
+                       |'>' -> Unequal((int_of_string hd1),
+                                       (int_of_string (String.sub hd2 1 (String.length hd2 -1))))
+                       |_ -> raise (Unvalid_atom str)
+                       end
+      |_ -> raise (Unvalid_atom str)
+      end
+    |_ -> raise (Unvalid_atom str)
+  with
+  |Failure _ -> raise (Unvalid_atom str)
+
+let atom_checker intmax str = match (atom_parser str) with
+  |Equal(x,y)|Unequal(x,y) as at when (0<x) && (0<y) && (x<=intmax) && (y<=intmax) -> at
+  |_ -> raise (Unvalid_atom str)
+
+              
+let clause_parser intmax str : clause =
+  try
+    List.rev_map (atom_checker intmax) (String.split_on_char ' ' str)
+  with
+  |Unvalid_atom s -> raise (Unvalid_line (str,s))
+                           
+let cnf_parser intmax nbclause ic linenum=
+  let rec aux ic acc nbline =
+    try 
+      let str = input_line ic in
+      match String.get str 0 with
+      |'c' -> aux ic acc (nbline+1)
+      |_ -> aux ic ((clause_parser intmax str)::acc) (nbline+1)
+    with
+    |Unvalid_line(line,atom) -> raise (Parsing_failed(nbline,Unvalid_line(line,atom)))
+    |End_of_file -> acc,nbline in
+  let cll,ln = aux ic [] linenum in
+  if (List.length cll == nbclause) then
+    {
+      nbvars = intmax;
+      clauses = cll;
+    }
+  else raise (Wrong_number_of_clauses ln)
+
+let file_parser path =
+  let rec start ic nbline =
+    try
+      let str = input_line ic in
+      match String.get str 0 with
+      |'c' -> start ic (nbline+1)
+      |'p' -> begin
+          match String.split_on_char ' ' str with
+          |p::cnf::nbvar::nbcl::[] -> (int_of_string nbvar,int_of_string nbcl,nbline)
+          |_ -> raise (Parsing_failed(nbline,Incorrect_initialization(str)))
+        end
+      |_ -> raise (Parsing_failed(nbline,Incorrect_initialization(str)))
+    with
+    |_ -> raise (Parsing_failed(nbline,Incorrect_initialization(""))) in
+  let mychan = open_in path in
+  let nbv,nbcl,nbl = start mychan 1 in
+  cnf_parser nbv nbcl mychan (nbl+1)
---
+++ unionfind.ml
@@ -0,0 +1,98 @@
+module type UnionFind = sig
+  type t
+  val create : int -> t
+  val find :  t -> int -> int
+  val union : t -> int  -> int -> t
+end
+
+module type PersistentArray = sig
+  type 'a t
+  val init : int -> (int -> 'a) -> 'a t
+  val get : 'a t -> int -> 'a
+  val set : 'a t -> int -> 'a -> 'a t
+end
+
+module Make(A : PersistentArray) : UnionFind = struct
+  type t = {
+      rank : int A.t;
+      mutable parent : int A.t;
+    }
+  let create n = {
+      rank = A.init n (fun _ -> 0);
+      parent = A.init n (fun k -> k);
+    }
+  let rec find_aux f i acc = match A.get f i with (* Tail-recursive *)
+    |finali when i == finali -> (List.fold_left (fun uf s -> A.set uf s finali) f acc),i
+    |tempi -> find_aux f tempi (i::acc)
+
+
+  let find h x =
+    let f,cx = find_aux h.parent x [] in
+    h.parent <- f;
+    cx
+
+  let union h x y =
+    let cx = find h x in
+    let cy = find h y in
+    if cx != cy then begin
+        let rx = A.get h.rank cx in
+        let ry = A.get h.rank cy in
+        if rx > ry then
+          {
+            h with parent = A.set h.parent cy cx
+          }
+        else if rx < ry then
+          {
+            h with parent = A.set h.parent cx cy
+          }
+        else
+          {
+            rank = A.set h.rank cx (rx + 1);
+            parent = A.set h.parent cy cx
+          }
+      end else
+      h
+end
+
+module PersArr : PersistentArray = struct
+  type 'a t = 'a data ref
+  and 'a data =
+    |Arr of 'a array
+    |Diff of int * 'a * 'a t
+
+  let init n f = ref (Arr (Array.init n f))
+  let rec reroot t = match !t with (* TODO : make it tail-recursive *)
+    |Arr _ -> ()
+    |Diff (i,v,t') ->
+      reroot t';
+      begin match !t' with
+      |Arr a as n -> let v' = a.(i) in
+                     a.(i) <- v;
+                     t := n;
+                     t' := Diff (i,v',t)
+      |Diff _ -> assert false
+      end
+
+  let rec get t i = match !t with
+    |Arr a -> a.(i)
+    |Diff _ ->
+      reroot t;
+      begin match !t with
+      |Arr a -> a.(i)
+      |Diff _ -> assert false
+      end
+
+        
+                                  
+  let set t i v = 
+    reroot t;
+    match !t with
+    |Arr a as n -> let old = a.(i) in
+                   a.(i) <- v;
+                   let res = ref n in
+                   t := Diff (i,old,res);
+                   res
+    |Diff _ -> assert false
+end
+
+                                     
